7808fdd50c7969d56f516e7bb1085cb03783d4b4
VisheshKG <heyvishesh@gmail.com>
2020-10-31 14:37:11 -0700

    Added base shooter methods. Can calculate angle of launcher and robot, but movements to those angles have not been completed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/UGoalRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/UGoalRobot.java
@@ -1,12 +1,94 @@
 package org.firstinspires.ftc.teamcode.ultimategoal;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.robot.MecabotMove;
 
 public class UGoalRobot extends MecabotMove {
+
     public UGoalRobot(HardwareMap ahwMap, LinearOpMode opMode) {
         super(ahwMap, opMode);
+        this.init(ahwMap);
+    }
+
+    // Motors
+    public DcMotor launcherMotor = null;
+
+
+    //Finals
+    public final double GOAL_HEIGHT = 48;
+    static final double ENCODER_TICKS_PER_ROTATION  = 537.6f; // goBilda 5202 series Yellow Jacket Planetary 19.2:1 gear ratio, 312 RPM
+
+
+    // The hardware map obtained from OpMode
+    HardwareMap hwMap;
+
+    // Initialization
+    public void init(HardwareMap ahwMap) {
+
+        launcherMotor = ahwMap.get(DcMotor.class, "launcherMotor");
+        launcherMotor.setDirection(DcMotor.Direction.REVERSE);
+        launcherMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        launcherMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+
+
+
+    // This assumes shooting at red goal
+    // This method gives the horizontal distance to goal, in other words the distance on the ground
+    public double distanceToShoot(int x, int y) {
+        double robotX = x;
+        double robotY = y;
+
+        double targetX = 72;
+        double targetY = -36;
+
+        double xDiff = targetX - robotX;
+        double yDiff = targetY - robotY;
+
+        return Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+    }
+
+    // This assumes shooting at red goal
+    // This method gives the angel from the robot to the goal
+    public double robotAngleToShoot(int x, int y) {
+        double robotX = x;
+        double robotY = y;
+
+        double targetX = 72;
+        double targetY = -36;
+
+        double xDiff = targetX - robotX;
+        double yDiff = targetY - robotY;
+
+
+        return Math.atan2(xDiff, yDiff);
+    }
+
+    public double launcherAngleToShoot(int x, int y) {
+        double xDist = distanceToShoot(x, y); //Distance on the ground
+        double yDist = GOAL_HEIGHT; // HEIGHT of the goal
+
+        return Math.atan(yDist/xDist);
+    }
+
+    public void shoot(int x, int y) {
+
+        odometryRotateToHeading(robotAngleToShoot(x, y), 0.5, 5, true);\
+
+        double launcherAngle = launcherAngleToShoot(x, y);
+
+        // MATH to covert angle to rotation of oval things under launcher
+        double ovalRotation = 0;
+
+        // Convertion of rotation to encoder ticks
+        double ovalRotationTicks = ENCODER_TICKS_PER_ROTATION / ovalRotation;
+
+        // Encoder movement for launcher motor (not drivetrain encoder methods)
+        launcherMotor.setTargetPosition((int) ovalRotationTicks);
+        launcherMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 }
